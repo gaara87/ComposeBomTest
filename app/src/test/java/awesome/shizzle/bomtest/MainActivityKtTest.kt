b5b85e6906c05230d624f7082667e9722c6b4515
akash.ramani <akash.ramani@robinhood.com>
2023-03-03 18:07:27 -0800

    Initial commit

---
+++ app/src/test/java/awesome/shizzle/bomtest/MainActivityKtTest.kt
@@ -0,0 +1,40 @@
+package awesome.shizzle.bomtest
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToString
+import com.jeppeman.mockposable.mockk.answersComposable
+import com.jeppeman.mockposable.mockk.everyComposable
+import com.jeppeman.mockposable.mockk.verifyComposable
+import io.mockk.mockkStatic
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainActivityKtTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun test() = mockkStatic("awesome.shizzle.bomtest.MainActivityKt") {
+        everyComposable {
+            Greeting(name = any(), modifier = any())
+        } answersComposable {
+            Text("ItsNotMe")
+        }
+        composeTestRule.setContent {
+            Greeting(name = "Itsame")
+        }
+        println(
+            composeTestRule.onRoot()
+                .printToString()
+        )
+        composeTestRule.onNodeWithText("ItsNotMe")
+            .assertIsDisplayed()
+    }
+}
